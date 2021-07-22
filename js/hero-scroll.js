3fd027a24282995c89f04800c3f180277881bab7
Sinonium <sinoniumx@gmail.com>
2021-07-22 18:45:57 +0600

    booking fetch, data done, some fixes

--- js/hero-scroll.js
+++ js/hero-scroll.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById('scroll-btn');
+const screenHeight = window.screen.height * 0.94
 
 btn.onclick = function () {
-    window.scrollTo(0, 1020);
+    window.scrollTo(0, screenHeight);
 }
